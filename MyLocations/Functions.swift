bbd4ef1d4eb479a102e20506b54f8316a5905ac6
Rachana Acharya <rasnacharya@gmail.com>
2015-08-06 22:46:59 -0600

    Moved the GCD code into a new file

--- MyLocations/Functions.swift
+++ MyLocations/Functions.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+import Dispatch
+
+// free function, not a method inside an object so can be used from anywhere in your code
+
+//afterDelay simply passes this closure object along to dispatch_after() 
+func afterDelay(seconds: Double, closure: () -> ()) {
+    let when = dispatch_time(DISPATCH_TIME_NOW, Int64(seconds * Double(NSEC_PER_SEC)))
+    dispatch_after(when, dispatch_get_main_queue(), closure)
+}
