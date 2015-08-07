5f5ccbf4bafa0d77f8425575d6a5f884a6aa002e
Rachana Acharya <rasnacharya@gmail.com>
2015-08-06 22:14:51 -0600

    Added line comments

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -44,6 +44,7 @@ class LocationDetailsViewController: UITableViewController {
         
         let delayInSeconds = 0.6
         let when = dispatch_time(DISPATCH_TIME_NOW, Int64(delayInSeconds * Double(NSEC_PER_SEC)))
+        //magic happens here dismisses the screen
         dispatch_after(when, dispatch_get_main_queue(), { self.dismissViewControllerAnimated(true, completion: nil) })
 //        println("Description '\(descriptionText)' ")
 //        dismissViewControllerAnimated(true, completion: nil)
