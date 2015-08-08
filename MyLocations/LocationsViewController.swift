eb46b0c472422b785c5f9fbdf36f1033e9c1997c
Rachana Acharya <rasnacharya@gmail.com>
2015-08-08 02:18:00 -0600

    Changed data source method; added logic to update description and address labels

--- MyLocations/LocationsViewController.swift
+++ MyLocations/LocationsViewController.swift
@@ -14,21 +14,57 @@ import CoreLocation
 class LocationsViewController: UITableViewController {
     var managedObjectContext: NSManagedObjectContext!
     
+    var locations = [Location]() //contains the list of Location objects
+    
     //MARK: - UITableViewDataSource
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return locations.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("LocationCell") as! UITableViewCell
         
+        let location = locations[indexPath.row]
+        
         let descriptionLabel = cell.viewWithTag(100) as! UILabel
-        descriptionLabel.text = "If you can see this"
+        descriptionLabel.text = location.locationDescription
         
         let addressLabel = cell.viewWithTag(101) as! UILabel
-        addressLabel.text = "Then it works!"
-        
+        if let placemark = location.placemark {
+            addressLabel.text = "\(placemark.subThoroughfare) \(placemark.thoroughfare)," +
+            "\(placemark.locality)"
+        } else {
+        addressLabel.text = " "
+        }
         return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //1. NSFetchRequest is the object that describes which objects we are going to fetch from the data store
+        // to retrieve an object saved in data store, create a fetch request that describes the search parameters of the object
+        let fetchRequest = NSFetchRequest()
+        
+        //2. tells the fetch request you are looking for entities
+        let entity = NSEntityDescription.entityForName("Location", inManagedObjectContext: managedObjectContext)
+        fetchRequest.entity = entity
+        
+        //3. tells the fetch request to sort on the date attribute in ascending order
+        let sortDescriptor = NSSortDescriptor(key: "date", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+       // fetch request complete
+        
+        //4.tell context to execute the fetch request
+        var error: NSError?
+        let foundObjects = managedObjectContext.executeFetchRequest(fetchRequest, error: &error)
+        if foundObjects == nil {
+            fatalCoreDataError(error)
+            return
+        }
+        
+        //5. if no error, assign the contents of the foundObjects array to the locations inst. var
+        locations = foundObjects as! [Location]
     }
 }
