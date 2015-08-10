672e453503efc177bb2f6c000a0e04c70730aa8c
Rachana Acharya <rasnacharya@gmail.com>
2015-08-09 19:11:02 -0600

    Added a new UITableViewCell subclass

---
+++ MyLocations/LocationCell.swift
@@ -0,0 +1,16 @@
+//
+//  LocationCell.swift
+//  MyLocations
+//
+//  Created by Rachana Acharya on 8/9/15.
+//  Copyright (c) 2015 Rachana Acharya. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+
+}
