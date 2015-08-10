44908b1a2a21a2e585d36d48d16994290c237199
Rachana Acharya <rasnacharya@gmail.com>
2015-08-09 22:17:49 -0600

    Added a method to put the location object into the table view cell

--- MyLocations/LocationCell.swift
+++ MyLocations/LocationCell.swift
@@ -12,5 +12,20 @@ class LocationCell: UITableViewCell {
 
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
+    
+    func configureForLocation(location: Location) {
+        if location.locationDescription.isEmpty {
+            descriptionLabel.text = "(No Description)"
+        } else {
+            descriptionLabel.text = location.locationDescription
+        }
+        
+        if let placemark = location.placemark {
+            addressLabel.text = "\(placemark.subThoroughfare) \(placemark.thoroughfare)," +
+            "\(placemark.locality)"
+        } else {
+            addressLabel.text = String(format: "Lat: %.8f", location.lattitude, location.longitude)
+        }
+    }
 
 }
