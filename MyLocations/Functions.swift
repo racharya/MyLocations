b3916a82a5ae2f660fc267a6f11b87d7c394f7ed
Rachana Acharya <rasnacharya@gmail.com>
2015-08-11 22:20:29 -0600

    Added a global constant to contain the path to app's Documents directory

--- MyLocations/Functions.swift
+++ MyLocations/Functions.swift
@@ -16,3 +16,11 @@ func afterDelay(seconds: Double, closure: () -> ()) {
     let when = dispatch_time(DISPATCH_TIME_NOW, Int64(seconds * Double(NSEC_PER_SEC)))
     dispatch_after(when, dispatch_get_main_queue(), closure)
 }
+
+//global constant named applicationDocu...ory to contain the path to the app's Document directory
+//uses lazy loading
+let applicationDocumentsDirectory: String = {
+    let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as! [String]
+    
+    return paths[0]
+}()
