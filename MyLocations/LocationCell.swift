fd46a8ecc83ac3653d7c72deb83fa93a87de4a31
Rachana Acharya <rasnacharya@gmail.com>
2015-08-12 00:47:54 -0600

    Modified configureForLocation method

--- MyLocations/LocationCell.swift
+++ MyLocations/LocationCell.swift
@@ -22,8 +22,11 @@ class LocationCell: UITableViewCell {
         }
         
         if let placemark = location.placemark {
-            addressLabel.text = "\(placemark.subThoroughfare) \(placemark.thoroughfare)," +
-            "\(placemark.locality)"
+            var text = ""
+            text.addText(placemark.subThoroughfare)
+            text.addText(placemark.thoroughfare, withSeparator: " ")
+            text.addText(placemark.locality, withSeparator: ", ")
+            addressLabel.text = text
         } else {
             addressLabel.text = String(format: "Lat: %.8f", location.lattitude, location.longitude)
         }
