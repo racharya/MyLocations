cfae339b061f160c3ccd9c2113596bb7f9fe0c37
Rachana Acharya <rasnacharya@gmail.com>
2015-08-12 01:17:45 -0600

    Added a new file to customize status bar

---
+++ MyLocations/MyNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  MyNavigationController.swift
+//  MyLocations
+//
+//  Created by Rachana Acharya on 8/12/15.
+//  Copyright (c) 2015 Rachana Acharya. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyNavigationController: UINavigationController {
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+    return .LightContent }
+}
