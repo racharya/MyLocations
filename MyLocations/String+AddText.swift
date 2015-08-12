ac39a85b89e5d84cf07182137724081c76feab2d
Rachana Acharya <rasnacharya@gmail.com>
2015-08-12 00:29:18 -0600

    Added a new file and moved addText method into it

---
+++ MyLocations/String+AddText.swift
@@ -0,0 +1,22 @@
+//
+//  String+AddText.swift
+//  MyLocations
+//
+//  Created by Rachana Acharya on 8/12/15.
+//  Copyright (c) 2015 Rachana Acharya. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    mutating func addText(text: String?,withSeparator separator: String) {
+        
+        if let text = text {
+        if !isEmpty {
+            self += separator
+        }
+        self += text
+        }
+    }
+}
